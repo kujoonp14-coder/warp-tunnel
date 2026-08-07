1f59e011c516bfd5933694c8f0990ffd9f7913eb
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-08-07 20:39:33 +0630

    Add Native Verification

--- app/src/main/java/com/myanmar/warpvpn/NativeUtils.kt
+++ app/src/main/java/com/myanmar/warpvpn/NativeUtils.kt
@@ -1,10 +1,13 @@
 package com.myanmar.warpvpn
+import android.content.Context
 
 object NativeUtils {
 
     init {
         System.loadLibrary("warpvpn")
     }
+    external fun verifyAppSignature(context: Context): Boolean
+    external fun validateLicenseNative(expireDateMillis: Long, isActivated: Boolean): Boolean
     external fun getCustomApiUrl(): String
     external fun getCfApiBase1(): String
     external fun getCfApiBase2(): String
